21547fef92d14306d30ab996e873aaaac2e16f73
Anastasia <anastasia.tuschina@yandex.ru>
2022-12-07 17:22:28 +0300

    installed gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
+const gulp = require("gulp");
+const gulpPlumber = require("gulp-plumber");
 const sass = require('gulp-sass')(require('sass'));
 
 const styles = (cb) => {
     return gulp.src('sourse/sass/style.scss')
-    .pipe(gulp.dest('sourse/css'))
+    .pipe(gulpPlumber())
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('sourse/css'))
+
   }
 
   exports.styles = styles;
