ff32659c554757b97a3cbc0a321a2482f922c09a
Anastasia <anastasia.tuschina@yandex.ru>
2022-12-07 18:10:39 +0300

    saving for previous commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 const gulp = require("gulp");
 const gulpPlumber = require("gulp-plumber");
-const sass = require('gulp-sass')(require('sass'));
+const sourcemap = require("gulp-sourcemaps");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
 
 const styles = (cb) => {
-    return gulp.src('sourse/sass/style.scss')
+    return gulp.src("sourse/sass/style.scss")
     .pipe(gulpPlumber())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('sourse/css'))
+    .pipe(sourcemap.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([
+      autoprefixer()
+    ]) )
+    .pipe(sourcemap.write("sourse/css"))
+    .pipe(gulp.dest("sourse/css"))
 
   }
 
